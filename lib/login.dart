7056e1a4a7a6da76cd5c83d142a9f78f59afbbd4
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-19 12:27:34 +0300

    Add name and password text fields

--- lib/login.dart
+++ lib/login.dart
@@ -6,14 +6,15 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // TODO: Add text editing controllers (101)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // SafeArea prevents Child from being hidden by display notch
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>[
+            // SizedBox is just a spacing
             SizedBox(height: 80.0),
             Column(
               children: <Widget>[
@@ -26,8 +27,24 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
             // TODO: Add button bar (101)
+            // [Name]
+            TextField(
+              decoration: InputDecoration(
+                filled: true, // fills the background to help the user identify input field
+                labelText: 'Username',
+              ),
+            ),
+            // spacer
+            SizedBox(height: 12.0),
+            // [Password]
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,  // replaces the input with bullets
+            ),
           ],
         ),
       ),
