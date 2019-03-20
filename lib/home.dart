05d8a10abf81be82dd243f32a38c7624b1392486
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-20 12:07:53 +0300

    Add products

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
-  // TODO: Make a collection of cards (102)
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -57,53 +60,77 @@ class HomePage extends StatelessWidget {
       // We use the count() constructor since the number of items to display
       // is countable and not infinite.
       body: GridView.count(
-        // The crossAxisCount: specifies how many items across. We want 2 columns.
-        // Cross axis in Flutter means the non-scrolling axis.
-        // The scrolling direction is called the main axis.
-        // (GridView has vertical scrolling by default)
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        children: _buildGridCards(10)
-      ),
+          // The crossAxisCount: specifies how many items across. We want 2 columns.
+          // Cross axis in Flutter means the non-scrolling axis.
+          // The scrolling direction is called the main axis.
+          // (GridView has vertical scrolling by default)
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(context)),
     );
   }
 
-  List<Card> _buildGridCards(int count) {
-    List<Card> cards = List.generate(
-      count,
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
       // Cards are independent elements that display content
       // and actions on a single subject.
       // The Card widget alone doesn't have enough information
       // to lay itself out where we could see it,
       // so we encapsulate it in a GridView widget.
-      (int index) => Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                // The AspectRatio widget decides what shape the image takes
-                // no matter what kind of image is supplied.
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Title'),
-                      SizedBox(height: 8.0),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                ),
-              ],
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // The AspectRatio widget decides what shape the image takes
+            // no matter what kind of image is supplied.
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // TODO: Adjust the box size (102)
+              ),
             ),
-          ),
-    );
-
-    return cards;
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product.name,
+                      style: theme.textTheme.title,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.body2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
   }
 }
